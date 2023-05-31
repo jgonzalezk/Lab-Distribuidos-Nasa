067b1b8247e0aaa7a368ec3288d7d372a2272444
Jorge <jorge.gonzalez.q@usach.cl>
2023-05-30 21:31:36 -0400

    Proyecto Spring Boot base

---
+++ src/test/java/com/api/nasaasteroidapi/NasaasteroidapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nasaasteroidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaasteroidapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
