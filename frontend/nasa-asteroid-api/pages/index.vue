1af63d3949f61fb4bdd28d8165c55990887848de
Jorge <jorge.gonzalez.q@usach.cl>
2023-06-25 03:22:57 -0400

    Add: Vista principal WIP

---
+++ frontend/nasa-asteroid-api/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="background-image">
+        
+    </div>
+</template>
+
+<style>
+.background-image::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: brightness(60%) blur(1.8px);
+    -webkit-backdrop-filter: brightness(60%) blur(1.8px);
+    pointer-events: none;
+}
+.background-image {
+    background-image: url("~/static/bg-astronaut.png");
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+}
+</style>
