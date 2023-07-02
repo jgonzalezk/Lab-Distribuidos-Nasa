34548f65c97fa7323cff9b536f190d5000574986
mauricioCastroB <mauricio.castro.b@usach.cl>
2023-07-02 00:18:12 -0400

    Creacion de servicios con spark

---
+++ backend/src/main/java/com/example/nasaasteroidapi/SparkConfig.java
@@ -0,0 +1,27 @@
+package com.example.nasaasteroidapi;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SparkConfig {
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession.builder()
+                .master("local")
+                .appName("MongoDBSparkApi")
+                .config("spark.mongodb.read.connection.uri", "mongodb://127.0.0.1/NASA_data.asteroids")
+                .config("spark.mongodb.write.connection.uri", "mongodb://127.0.0.1/NASA_data.asteroids")
+                .getOrCreate();
+
+    }
+
+    @Bean
+    public JavaSparkContext javaSparkContext() {
+        return new JavaSparkContext(sparkSession().sparkContext());
+    }
+
+
+}
--- backend/src/main/java/com/example/nasaasteroidapi/service/AsteroideService.java
+++ backend/src/main/java/com/example/nasaasteroidapi/service/AsteroideService.java
@@ -1,28 +1,104 @@
 package com.example.nasaasteroidapi.service;
-import java.util.List;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import java.util.List;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.apache.spark.SparkContext;
-import org.apache.spark.SparkConf;
 import com.example.nasaasteroidapi.model.Asteroide;
 import com.example.nasaasteroidapi.repository.AsteroideRepository;
 
+import static org.apache.spark.sql.functions.*;
 import lombok.RequiredArgsConstructor;
-
 
 @Service
 @RequiredArgsConstructor
 public class AsteroideService {
     private final AsteroideRepository asteroideRepository;
-    //private final JavaSparkContext javaSparkContext;
-    SparkConf conf = new SparkConf().setAppName("api").setMaster("local");
-    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    @Autowired
+    private SparkSession spark;
 
     public List<Asteroide> getAll() {
         return asteroideRepository.findAll();
     }
-}
 
+    public Long total_asteroide(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Long total = df
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .count();
+        return total;
+
+    }
+
+    public Long total_peligroso(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Long result = df
+                .where("Is_potentially_hazardous_asteroid = true")
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .count();
+        return result;
+    }
+
+    public Double velocidad_promedio(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Double result = df
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Relative_velocity_KpS"))
+                .first()
+                .getDouble(0);
+        return result;
+
+    }
+
+    public Double velocidad_promedio_peligroso(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Double result = df
+                .where("Is_potentially_hazardous_asteroid = true")
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Relative_velocity_KpS"))
+                .first()
+                .getDouble(0);
+        return result;
+
+    }
+
+    public Double tamano_promedio(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Double resultMin = df
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Estimated_diameter_met_min"))
+                .first()
+                .getDouble(0);
+        Double resultMax = df
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Estimated_diameter_met_max"))
+                .first()
+                .getDouble(0);
+        Double result = (resultMax + resultMin) / 2;
+        return result;
+
+    }
+
+    public Double tamano_promedio_peligroso(Column fechaInicial, Column fechaFinal) {
+        Dataset<Row> df = spark.read().format("mongodb").load();
+        Double resultMin = df
+                .where("Is_potentially_hazardous_asteroid = true")
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Estimated_diameter_met_min"))
+                .first()
+                .getDouble(0);
+        Double resultMax = df
+                .where("Is_potentially_hazardous_asteroid = true")
+                .where(col("Date").geq(fechaInicial).and(col("Date").leq(to_date(lit(fechaFinal), "yyyy-MM-dd"))))
+                .agg(avg("Estimated_diameter_met_max"))
+                .first()
+                .getDouble(0);
+        Double result = (resultMax + resultMin) / 2;
+        return result;
+
+    }
+}
