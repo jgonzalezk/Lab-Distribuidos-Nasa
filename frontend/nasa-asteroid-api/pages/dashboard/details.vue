762656878be3e98bc7fb0d28c1e8102674865a97
Jorge <jorge.gonzalez.q@usach.cl>
2023-07-08 20:57:27 -0400

    dockerizar el front y cambios en vistas

--- frontend/nasa-asteroid-api/pages/dashboard/details.vue
+++ frontend/nasa-asteroid-api/pages/dashboard/details.vue
@@ -5,13 +5,13 @@
         <a href="/" class="flex"><img src="~/assets/banner.png" alt="logo" class="mx-auto h-20 w-auto"></a>
         <a href="/dashboard" class="tracking-wider uppercase text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Información General</a>
         <a href="#" class="tracking-wider uppercase text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Información Detallada</a>
-        <h1 class="text-white">Desde</h1>
+        <h1 class="text-white">Desde:</h1>
         <VueDatePicker v-model="dateIni" placeholder="Comienzo del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es" />
-        <h1 class="text-white">Hasta</h1>
-        <VueDatePicker v-model="dateFin" placeholder="Comienzo del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es" />
+        <h1 class="text-white">Hasta:</h1>
+        <VueDatePicker v-model="dateFin" placeholder="Fin del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es" />
         <div class="flex items-center space-x-5">
           <a v-on:click="console.log('sdfsd')" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Filtrar</a>
-          <a v-on:click="console.log('sdfsd')" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Limpiar filtro</a>
+          <a v-on:click="limpiar_filtro" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Limpiar filtro</a>
         </div>
       </div>
     </aside>
@@ -30,12 +30,18 @@
                 <p><strong>Velocidad relativa:</strong> {{ item.velocity }}</p>
               </div>
               <div>
-                <span class="bg-purple-300 text-purple-700 rounded-3xl px-3 font-bold">{{ item.date }}</span>
+                <span class="bg-[#6AD9D1] text-[#2C5956] rounded-3xl px-3 font-bold">{{ item.date }}</span>
                 <p><strong>Cuerpo que Orbita:</strong> {{ item.orbit }}</p>
               </div>
             </div>
-            <div class="flex items-center">
-              <div :class="['w-8 h-8 rounded-full', item.status ? 'bg-green-500' : 'bg-red-500']"></div>
+            <div class="flex items-center relative">
+              <div class="w-8 h-8 rounded-full" :class="[ item.status ? 'bg-green-500' : 'bg-red-500']"
+                @mouseover="item.hovered = true"
+                @mouseleave="item.hovered = false"
+              ></div>
+              <span v-if="item.hovered" class="absolute right-10 -ml-40 bg-gray-800 text-white px-2 py-1 rounded">
+                {{ item.status ? 'No es potencialmente peligroso' : 'Potencialmente peligroso' }}
+              </span>
             </div>
           </li>
         </ul>
@@ -77,6 +83,10 @@ export default {
 
   },
   methods: {
+    limpiar_filtro() {
+      this.dateIni = '';
+      this.dateFin = '';
+    },
   },
   async mounted() {
     // const response = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY');
--- frontend/nasa-asteroid-api/pages/dashboard/index.vue
+++ frontend/nasa-asteroid-api/pages/dashboard/index.vue
@@ -5,38 +5,28 @@
                 <a href="/" class="flex"><img src="~/assets/banner.png" alt="logo" class="mx-auto h-20 w-auto"></a>
                 <a href="#" class="tracking-wider uppercase text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Información General</a>
                 <a href="/dashboard/details" class="tracking-wider uppercase text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Información Detallada</a>
-                <h1 class="text-white">Desde</h1>
+                <h1 class="text-white">Desde:</h1>
                 <VueDatePicker v-model="dateIni" placeholder="Comienzo del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es"/>
-                <h1 class="text-white">Hasta</h1>
-                <VueDatePicker v-model="dateFin" placeholder="Comienzo del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es"/>
+                <h1 class="text-white">Hasta:</h1>
+                <VueDatePicker v-model="dateFin" placeholder="Fin del periodo" model-date="dd.MM.yyyy" :enable-time-picker="false" locale="es"/>
                 <div class="flex items-center space-x-5">
                     <a v-on:click="console.log('sdfsd')" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Filtrar</a>
-                    <a v-on:click="console.log('sdfsd')" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Limpiar filtro</a>
+                    <a v-on:click="limpiar_filtro" class="tracking-wider text-center bg-gray-800 ring-2 ring-[#66fcf1] text-white px-4 py-2 mb-2 font-bold hover:bg-gray-700 rounded-full">Limpiar filtro</a>
                 </div>
             </div>
         </aside>
-
-        <main class="flex-grow bg-space p-4">
+        
+        <main class="flex-grow bg-space p-4 flex items-center justify-center w-max">
             <!-- Contenido principal -->
-            <div class="grid grid-cols-4 gap-4 justify-center">
+            <div class="grid grid-cols-4 gap-4 w-full max-w-screen-lg mx-auto">
+                <h1 class="text-3xl leading-tight text-white font-bold mb-4 text-center col-span-4">Información general promedio del período</h1>
+    <!-- Tarjetas 1-8 -->
                 <!-- Tarjetas 1-8 -->
-                <div v-for="(tarjeta,index) in tarjetas" :key="index" class='flex flex-wrap flex-row sm:flex-col justify-center items-center w-full p-5 bg-white rounded-md shadow-xl border-l-4 border-blue-300'>
-		            <div class="flex justify-between w-full">
-			            <div>
-				            <div class="p-2">
-					            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-						        <path stroke-linecap="round" stroke-linejoin="round"
-							        d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-					            </svg>
-				            </div>
-			            </div>
-                        <div>
-                            <div style="padding-top: 0.1em; padding-bottom: 0.1rem" class="flex items-center text-xs px-3 bg-blue-200 text-blue-800 rounded-full">100%</div>
-                        </div>
-		            </div>
-		            <div>
-                        <div class="font-bold text-5xl">{{tarjeta.valor}}</div>
-                        <div class="font-bold text-sm">{{tarjeta.nombre}}</div>
+                <div v-for="(tarjeta,index) in tarjetas" :key="index" :class="{'border-green-500': !tarjeta.estado,'border-red-500': tarjeta.estado}" class='flex flex-wrap flex-row sm:flex-col justify-center items-center w-full p-5 py-6 bg-white rounded-md shadow-xl border-l-[5px] border-blue-300'>
+		            <div class="text-center">
+                        <div class="font-bold text-gray-800 text-5xl">{{tarjeta.valor}}</div>
+                        <div class="font-bold text-gray-800 text-base">{{tarjeta.nombre}}</div>
+                        <div class="font-bold text-gray-800 text-sm">{{ tarjeta.estado ? 'Potencialmente Peligrosos' : 'Inofensivos' }}</div>
 		            </div>
 	            </div>
             </div>
@@ -67,14 +57,14 @@ export default {
     data(){
         return{
             tarjetas: [
-                {nombre: 'Asteroides', valor: 0},
-                {nombre: 'Cometas', valor: 0},
-                {nombre: 'Meteoritos', valor: 0},
-                {nombre: 'Meteoros', valor: 0},
-                {nombre: 'Meteoroides', valor: 0},
-                {nombre: 'Satélites', valor: 0},
-                {nombre: 'Estrellas fugaces', valor: 0},
-                {nombre: 'Estrellas', valor: 0},
+                {nombre: 'Asteroides', valor: 0, estado: false},
+                {nombre: 'Cometas', valor: 0, estado: false},
+                {nombre: 'Meteoritos', valor: 0, estado: false},
+                {nombre: 'Meteoros', valor: 0, estado: false},
+                {nombre: 'Meteoroides', valor: 0, estado: true},
+                {nombre: 'Satélites', valor: 0, estado: true},
+                {nombre: 'Estrellas fugaces', valor: 0, estado: true},
+                {nombre: 'Estrellas', valor: 0, estado: true},
             ]
         }
     },
@@ -82,6 +72,10 @@ export default {
 
     },
     methods:{
+        limpiar_filtro() {
+            this.dateIni = '';
+            this.dateFin = '';
+        },
     },
     async mounted(){
         const response = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY');
