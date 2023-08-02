d7865052c5a2c26d03822b04efd748ea60a77b76
Jorge <jorge.gonzalez.q@usach.cl>
2023-08-01 22:13:58 -0400

    dfdfssdfdfs

--- backend/src/main/java/com/example/nasaasteroidapi/SparkConfig.java
+++ backend/src/main/java/com/example/nasaasteroidapi/SparkConfig.java
@@ -17,10 +17,10 @@ public class SparkConfig {
                 // .config("spark.mongodb.write.connection.uri", "mongodb://host.docker.internal:27017/NASA_data.asteroids")
                 // .config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
                 // .config("spark.mongodb.write.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
-                .config("spark.mongodb.read.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@host.docker.internal:27017/NASA_data.asteroids?authSource=admin")
-                .config("spark.mongodb.write.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@host.docker.internal:27017/NASA_data.asteroids?authSource=admin")
-                // .config("spark.mongodb.read.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@localhost:27017/NASA_data.asteroids?authSource=admin")
-                // .config("spark.mongodb.write.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@localhost:27017/NASA_data.asteroids?authSource=admin")
+                // .config("spark.mongodb.read.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@host.docker.internal:27017/NASA_data.asteroids?authSource=admin")
+                // .config("spark.mongodb.write.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@host.docker.internal:27017/NASA_data.asteroids?authSource=admin")
+                .config("spark.mongodb.read.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@localhost:27017/NASA_data.asteroids?authSource=admin")
+                .config("spark.mongodb.write.connection.uri", "mongodb://Nasa_user_name:Nasa_secret_password@localhost:27017/NASA_data.asteroids?authSource=admin")
                 .getOrCreate();
 
     }
