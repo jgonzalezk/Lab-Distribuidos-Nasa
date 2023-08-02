f3be1a7026f2db03f849c57b38fbd96f9fe5f841
mauricioCastroB <64172072+mauricioCastroB@users.noreply.github.com>
2023-08-01 20:24:32 -0400

    Update SparkConfig.java

--- backend/src/main/java/com/example/nasaasteroidapi/SparkConfig.java
+++ backend/src/main/java/com/example/nasaasteroidapi/SparkConfig.java
@@ -10,12 +10,13 @@ public class SparkConfig {
     @Bean
     public SparkSession sparkSession() {
         return SparkSession.builder()
+            //  String mongoURI = "mongodb://" + mongoUsername + ":" + mongoPassword + "@localhost:27017/NASA_data.asteroids";
                 .master("local")
                 .appName("MongoDBSparkApi")
-                //.config("spark.mongodb.read.connection.uri", "mongodb://host.docker.internal:27017/NASA_data.asteroids")
-                //config("spark.mongodb.write.connection.uri", "mongodb://host.docker.internal:27017/NASA_data.asteroids")
-                .config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
-                .config("spark.mongodb.write.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
+                .config("spark.mongodb.read.connection.uri", "mongodb://MONGO_USERNAME:MONGO_PASSWORD@host.docker.internal:27017/NASA_data.asteroids")
+                .config("spark.mongodb.write.connection.uri", "mongodb://MONGO_USERNAME:MONGO_PASSWORD@host.docker.internal:27017/NASA_data.asteroids")
+                //.config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
+                //.config("spark.mongodb.write.connection.uri", "mongodb://localhost:27017/NASA_data.asteroids")
                 .getOrCreate();
 
     }
